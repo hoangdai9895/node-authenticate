bf962880ab4ddefbd5250695792a848429129451
unknown <hoangdai9895@gmail.com>
2019-08-28 16:33:47 +0700

    config database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb+srv://admin:buihaiyen@video-idea-hiuts.gcp.mongodb.net/mean-1?retryWrites=true&w=majority",
+    databse: "mongodb+srv://admin:yourdatabase",
     secret: "secret"
 }
